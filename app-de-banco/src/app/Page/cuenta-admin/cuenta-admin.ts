11075cc34d117c33bddf54c5902af947448fb4dc
Luis-bal <baldiviesoviadezp@gmail.com>
2025-12-16 02:32:24 -0400

    ahorasielcrud

--- app-de-banco/src/app/Page/cuenta-admin/cuenta-admin.ts
+++ app-de-banco/src/app/Page/cuenta-admin/cuenta-admin.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { ClienteNew } from '../../componets/cliente-new/cliente-new';
+import { ModalFromcreaus } from '../../componets/modal-fromcreaus/modal-fromcreaus';
 
 @Component({
   selector: 'page-cuenta-admin',
-  imports: [ClienteNew],
+  imports: [CommonModule, ClienteNew, ModalFromcreaus],
   templateUrl: './cuenta-admin.html'
 })
-export class CuentaAdmin {}
+export class CuentaAdmin {
+  abrirModalCuenta = false;
+
+  abrirModal() {
+    this.abrirModalCuenta = true;
+  }
+
+  cerrarModal() {
+    this.abrirModalCuenta = false;
+  }
+}
+
---
+++ app-de-banco/src/app/componets/modal-fromcreaus/modal-fromcreaus.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-modal-fromcreaus',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './modal-fromcreaus.html',
+})
+export class ModalFromcreaus {}
